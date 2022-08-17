f89728f7c8e8c98a66d0723649814e80a26c1244
routb68 <101084372+routb68@users.noreply.github.com>
2022-08-17 19:14:32 +0530

    Time: 37 ms (11.93%), Space: 8.5 MB (84.55%) - LeetHub

---
+++ 1031-maximum-sum-of-two-non-overlapping-subarrays/1031-maximum-sum-of-two-non-overlapping-subarrays.cpp
@@ -0,0 +1,47 @@
+class Solution {
+public:
+    int maxSumTwoNoOverlap(vector<int>& nums, int fln, int sln) {
+        int res=INT_MIN;
+        if(fln+sln==nums.size())return accumulate(begin(nums),end(nums),0);
+        for(int i=0;i<=nums.size()-sln-fln;++i){
+            int sum1=0,sum2=0;
+            for(int j=i;j<i+fln;++j){
+                sum1+=nums[j];
+            }
+            for(int j=i+fln;j<i+fln+sln;++j){
+                sum2+=nums[j];
+            }
+            res=max(res,sum1+sum2);
+            int low=i+fln,high=i+fln+sln;
+            while(high<nums.size()){
+                res=max(res,sum1+sum2);
+                sum2-=nums[low];
+                ++low;
+                sum2+=nums[high];
+                ++high;
+            }
+            res=max(res,sum1+sum2);
+        }
+        swap(fln, sln);
+        for(int i=0;i<=nums.size()-sln-fln;++i){
+            int sum1=0,sum2=0;
+            for(int j=i;j<i+fln;++j){
+                sum1+=nums[j];
+            }
+            for(int j=i+fln;j<i+fln+sln;++j){
+                sum2+=nums[j];
+            }
+            res=max(res,sum1+sum2);
+            int low=i+fln,high=i+fln+sln;
+            while(high<nums.size()){
+                res=max(res,sum1+sum2);
+                sum2-=nums[low];
+                ++low;
+                sum2+=nums[high];
+                ++high;
+            }
+            res=max(res,sum1+sum2);
+        }
+        return res;
+    }
+};
