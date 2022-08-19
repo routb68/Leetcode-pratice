29b445a1de07acd7a2841c8e146824f800d74c63
routb68 <101084372+routb68@users.noreply.github.com>
2022-08-19 09:02:06 +0530

    Time: 70 ms (9.49%), Space: 7 MB (85.18%) - LeetHub

--- 424-longest-repeating-character-replacement/424-longest-repeating-character-replacement.cpp
+++ 424-longest-repeating-character-replacement/424-longest-repeating-character-replacement.cpp
@@ -1,17 +1,22 @@
 class Solution {
 public:
-    int characterReplacement(string s, int k) {
-        int res=0,maxf=0;
-        vector<int>cnt(26,0);
-        for(int i=0;i<s.length();++i){
-            ++cnt[s[i]-'A'];
-            maxf=max(maxf,cnt[s[i]-'A']);
-            if(res-maxf<k){
-                ++res;
-            }else{
-                cnt[s[i-res]-'A']--;
-            }
-        }
-        return res;
-    }
+	int characterReplacement(string s, int k, int ans = 0) {
+		int c[26] = {0};
+		int l = 0;
+		function<bool(int)> ok = [&](int len) {
+			int maxf = *max_element(c, c + 26);
+			return (maxf + k >= len);
+		};
+		for (int r = 0; r < s.size(); ++r) {
+			c[s[r] - 'A']++;
+			while (ok(r - l + 1) == false) {
+				c[s[l] - 'A']--;
+				l++;
+			}
+			if (ok(r - l + 1)) {
+				ans = max(ans, r - l + 1);
+			}
+		}
+		return ans;
+	}
 };
