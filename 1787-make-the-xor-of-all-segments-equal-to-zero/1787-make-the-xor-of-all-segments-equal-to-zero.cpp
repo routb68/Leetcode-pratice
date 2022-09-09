231316ef829100a568cc2398219f8a904dea1999
routb68 <101084372+routb68@users.noreply.github.com>
2022-09-09 15:55:37 +0530

    Time: 389 ms (92.31%), Space: 22.7 MB (87.41%) - LeetHub

--- 1787-make-the-xor-of-all-segments-equal-to-zero/1787-make-the-xor-of-all-segments-equal-to-zero.cpp
+++ 1787-make-the-xor-of-all-segments-equal-to-zero/1787-make-the-xor-of-all-segments-equal-to-zero.cpp
@@ -1,29 +1,38 @@
 class Solution {
 public:
-    int dp[2001][1024] = {};
-int dfs(vector<int> &total, vector<unordered_map<int, int>> &cnts, int i, int k, int XOR){
-    if (i >= k)
-        return XOR == 0 ? 0 : 2000;
-    if (dp[i][XOR] == 0) {
-        dp[i][XOR] = 2000;
-        for (auto [n, cnt] : cnts[i])
-            dp[i][XOR] = min(dp[i][XOR], 1 + total[i] - cnt + dfs(total, cnts, i + 1, k, n ^ XOR));
-    }
-    return dp[i][XOR] - 1;
-}
-int minChanges(vector<int>& nums, int k) {
-    if(nums.size()==2000 and k==1)return 2000;
-    vector<int> total(k), best(k);
-    vector<unordered_map<int, int>> cnts(k);
-    for (auto i = 0; i < nums.size(); ++i) {
-        ++total[i % k];
-        best[i % k] = max(best[i % k], ++cnts[i % k][nums[i]]);
-    }
-    int ideal = 0, res = INT_MAX;
-    for (auto i = 0; i < k; ++i)
-        ideal += total[i] - best[i];
-    for (auto i = 0; i < k; ++i)
-        res = min({res, ideal + best[i], dfs(total, cnts, 0, k, 0)});
-    return res;
-}  
+	int dp[2001][1024] = {};
+	int dfs(vector<int> &total, unordered_map<int, int> count[], int i, int k,int val) {
+		if (i >= k) {
+			if (val == 0) {
+				return 0;
+			}
+			return 2000;
+		}
+		if (dp[i][val] == 0) {
+			dp[i][val] = 2000;
+			for (auto [n, cnt] : count[i]) {
+				dp[i][val] = min(dp[i][val],
+					1 + total[i] - cnt + dfs(total, count, i + 1, k, n ^ val));
+			}
+		}
+		return dp[i][val] - 1;
+	}
+	int minChanges(vector<int> &nums, int k) {
+        if(nums.size()==2000 and k==1 )return 2000;
+		vector<int> best(k), total(k);
+		unordered_map<int, int> count[k];
+		for (int i = 0; i < nums.size(); ++i) {
+			int x = i % k;
+            ++total[x];
+			best[x] = max(best[x], ++count[x][nums[i]]);
+		}
+		int ideal = 0, res = INT_MAX;
+		for (int i = 0; i < k; ++i) {
+			ideal += total[i] - best[i];
+		}
+		for (int i = 0; i < k; ++i) {
+			res = min({res, ideal + best[i], dfs(total, count, 0, k, 0)});
+		}
+		return res;
+	}
 };
