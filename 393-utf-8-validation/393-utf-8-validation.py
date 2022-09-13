86ced0a7ac3f62c244f1ad3b054e10360d4290a7
routb68 <101084372+routb68@users.noreply.github.com>
2022-09-13 17:33:43 +0530

    Time: 194 ms (46.20%), Space: 14.1 MB (96.64%) - LeetHub

---
+++ 393-utf-8-validation/393-utf-8-validation.py
@@ -0,0 +1,22 @@
+class Solution:
+    def validUtf8(self, data: List[int]) -> bool:
+        def check(num):
+            mask = 1 << (8 - 1)  
+            i = 0
+            while num & mask: 
+                mask >>= 1
+                i += 1
+            return i
+
+        i = 0
+        while i < len(data):
+            j = check(data[i])
+            k = i + j - (j != 0)
+            i += 1
+            if j == 1 or j > 4 or k >= len(data):
+                return False
+            while i < len(data) and i <= k:
+                cur = check(data[i])
+                if cur != 1: return False
+                i += 1
+        return True
