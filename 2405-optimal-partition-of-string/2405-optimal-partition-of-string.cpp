078abe153b02194572cb988300aaa12485fb0732
routb68 <101084372+routb68@users.noreply.github.com>
2022-09-17 00:11:42 +0530

    Time: 57 ms (89.33%), Space: 10.1 MB (97.23%) - LeetHub

--- 2405-optimal-partition-of-string/2405-optimal-partition-of-string.cpp
+++ 2405-optimal-partition-of-string/2405-optimal-partition-of-string.cpp
@@ -1,14 +1,14 @@
 class Solution {
 public:
+    int v[26];
     int partitionString(string s) {
-        vector<int>v(26,0);
         int ans=0;
         for(char &x:s){
             if(v[x-'a']==0){
                 ++v[x-'a'];
             }else{
                 ++ans;
-                for(int i=0;i<26;++i) v[i]=0;
+                memset(v, 0, sizeof v);
                 ++v[x-'a'];
             }
         }
