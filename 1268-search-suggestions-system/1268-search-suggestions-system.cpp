840a513c837bd5537bfd805a24d115c3f1170cd3
routb68 <101084372+routb68@users.noreply.github.com>
2022-09-17 13:49:28 +0530

    Time: 201 ms (44.33%), Space: 74.2 MB (47.62%) - LeetHub

---
+++ 1268-search-suggestions-system/1268-search-suggestions-system.cpp
@@ -0,0 +1,69 @@
+class Solution {
+public:
+    struct TrieNode{
+        TrieNode* child[26];
+        bool end;
+        TrieNode(){
+            end=false;
+            for(int i=0;i<26;++i){
+                child[i]=NULL;
+            }
+        }   
+    };
+    void insert(string &s){
+            TrieNode* curr=root;
+            for(char &x:s){
+                int idx=x-'a';
+                if(!curr->child[idx]){
+                    curr->child[idx]=new TrieNode();
+                }
+                curr=curr->child[idx];
+            }
+            curr->end=true;
+    }
+    void com(string &se){
+        TrieNode* curr=root;
+        string s="";
+        for(char &x:se){
+            int idx=x-'a';
+            if(curr->child[idx]!=NULL){
+                s+=x;
+                ans.clear();
+                add(curr->child[idx],s);
+                res.push_back(ans);
+                curr=curr->child[idx];
+            }else{
+                return;
+            }
+        }
+    }
+    void add(TrieNode *n,string &s){
+        if(ans.size()==3) return;
+        if(n->end==true) ans.push_back(s);
+        for(int i=0;i<26;++i){
+            if(n->child[i]!=NULL){
+                s+=char('a'+i);
+                add(n->child[i],s);
+                s.pop_back();
+            }
+        }
+    }
+    
+    TrieNode* root;
+    vector<vector<string>>res;
+    vector<string>ans;
+    vector<vector<string>> suggestedProducts(vector<string>& p, string se) {
+        root=new TrieNode();
+        for(string &x:p){
+            insert(x);
+        }
+        com(se);
+        if(res.size()<se.length()){
+            while(res.size()<se.size()){
+                res.push_back({});
+            }
+        }
+        return res;
+        
+    }
+};
