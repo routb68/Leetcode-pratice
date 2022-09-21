41522a68fa570feb13fd34e584cbe2e05d526991
routb68 <101084372+routb68@users.noreply.github.com>
2022-09-21 11:05:25 +0530

    Time: 295 ms (8.14%), Space: 45.4 MB (90.67%) - LeetHub

--- 985-sum-of-even-numbers-after-queries/985-sum-of-even-numbers-after-queries.cpp
+++ 985-sum-of-even-numbers-after-queries/985-sum-of-even-numbers-after-queries.cpp
@@ -9,21 +9,12 @@ public:
         }
         vector<int>ans(q.size());
         for(int i=0;i<q.size();++i){
-            if((nums[q[i][1]]&1)==0){
-                if((q[i][0] & 1)==0){
-                    res+=q[i][0];
-                    nums[q[i][1]]+=q[i][0];
-                }else{
-                    res-=nums[q[i][1]];
-                    nums[q[i][1]]+=q[i][0];
-                }
-            }else{
-                if((q[i][0] & 1)==0){
-                    nums[q[i][1]]+=q[i][0];
-                }else{
-                    nums[q[i][1]]+=q[i][0];
-                    res+=nums[q[i][1]];
-                }
+            if((nums[q[i][1]] & 1)==0){
+                res-=nums[q[i][1]];
+            }
+            nums[q[i][1]]+=q[i][0];
+            if((nums[q[i][1]] & 1)==0){
+                res+=nums[q[i][1]];
             }
             ans[i]=res;
         }
