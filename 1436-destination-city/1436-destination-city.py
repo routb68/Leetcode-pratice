9327df98ea72c9395db15827c0b37ccabbfd9dbe
routb68 <101084372+routb68@users.noreply.github.com>
2022-09-23 01:05:52 +0530

    Time: 97 ms (39.71%), Space: 14 MB (6.58%) - LeetHub

--- 1436-destination-city/1436-destination-city.py
+++ 1436-destination-city/1436-destination-city.py
@@ -1,11 +1,4 @@
 class Solution:
     def destCity(self, paths: List[List[str]]) -> str:
-        num_cnt = {}
-        for p in paths:
-            city1,city2=p
-            num_cnt[city1] = num_cnt.get(city1,0)+1
-            
-            num_cnt[city2] = num_cnt.get(city2,0)
-        for city in num_cnt:
-            if num_cnt[city]==0:
-                return city;
+        A, B = map(set, zip(*paths))
+        return (B - A).pop()
