64596779f5244d1de53d05dcd0eb8a68ff5eed55
Biswajit Rout <101084372+routb68@users.noreply.github.com>
2022-12-03 05:57:51 +0530

    Time: 98 ms (64.56%), Space: 28.1 MB (99.15%) - LeetHub

---
+++ 0561-array-partition/0561-array-partition.cpp
@@ -0,0 +1,9 @@
+class Solution {
+public:
+    int arrayPairSum(vector<int>& nums) {
+        sort(begin(nums),end(nums));
+        int ans=0;
+        for(int i=0;i<nums.size();i=i+2) ans+=nums[i];
+        return ans;
+    }
+};
