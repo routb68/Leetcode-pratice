d29d8570055c96943c845072a2325b17ca783883
Biswajit Rout <101084372+routb68@users.noreply.github.com>
2022-12-06 20:17:30 +0530

    Time: 16 ms (6.24%), Space: 12.2 MB (37.12%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.cpp
+++ 0031-next-permutation/0031-next-permutation.cpp
@@ -1,6 +1,16 @@
 class Solution {
 public:
     void nextPermutation(vector<int>& nums) {
-        next_permutation(begin(nums),end(nums));
+        int idx=nums.size()-1;
+        while(idx>0 and nums[idx-1]>=nums[idx]) --idx;
+        --idx;
+        if(idx<0){
+            reverse(begin(nums),end(nums));
+            return;
+        }
+        int a=nums.size()-1;
+        while(nums[a]<=nums[idx])--a;
+        swap(nums[a],nums[idx]);
+        reverse(begin(nums)+idx+1,end(nums));
     }
 };
