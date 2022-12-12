5e9dfcc27e2b6e60119737ec075cd08c1d612f5d
Biswajit Rout <101084372+routb68@users.noreply.github.com>
2022-12-13 02:59:40 +0530

    Time: 0 ms (100.00%), Space: 6.3 MB (50.88%) - LeetHub

--- 1638-count-substrings-that-differ-by-one-character/1638-count-substrings-that-differ-by-one-character.cpp
+++ 1638-count-substrings-that-differ-by-one-character/1638-count-substrings-that-differ-by-one-character.cpp
@@ -2,13 +2,26 @@ class Solution {
 public:
     int countSubstrings(string s, string t) {
         int res=0;
+        int dpl[101][101]={};
+        int dpr[101][101]={};
+        for(int i=1;i<=s.length();++i){
+            for(int j=1;j<=t.length();++j){
+                if(s[i-1]==t[j-1]){
+                    dpl[i][j]=1+dpl[i-1][j-1];
+                }
+            }
+        }
+        for(int i=s.length();i>0;--i){
+            for(int j=t.length();j>0;--j){
+                if(s[i-1]==t[j-1]){
+                    dpr[i-1][j-1]=1+dpr[i][j];
+                }
+            }
+        }
         for(int i=0;i<s.length();++i){
             for(int j=0;j<t.length();++j){
                 if(s[i]!=t[j]){
-                    int l=1,r=1;
-                    while(i-l>=0 and j-l>=0 and s[i-l]==t[j-l])++l;
-                    while(i+r<s.length() and j+r<t.length() and s[i+r]==t[j+r])++r;
-                    res+= l*r;
+                    res+=(dpl[i][j]+1)*(dpr[i+1][j+1]+1);
                 }
             }
         }
