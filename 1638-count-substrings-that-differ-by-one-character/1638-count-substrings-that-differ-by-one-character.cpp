c04c4da7d9c0179c7cf852d7f02221fe54e6f9a2
Biswajit Rout <101084372+routb68@users.noreply.github.com>
2022-12-13 02:43:34 +0530

    Time: 5 ms (62.22%), Space: 6.1 MB (86.15%) - LeetHub

--- 1638-count-substrings-that-differ-by-one-character/1638-count-substrings-that-differ-by-one-character.cpp
+++ 1638-count-substrings-that-differ-by-one-character/1638-count-substrings-that-differ-by-one-character.cpp
@@ -4,10 +4,11 @@ public:
         int res=0;
         for(int i=0;i<s.length();++i){
             for(int j=0;j<t.length();++j){
-                int miss=0;
-                for(int k=0;k+i<s.length() and k+j<t.length();++k){
-                    if(s[i+k]!=t[k+j] and ++miss>1) break;
-                    res+=miss;
+                if(s[i]!=t[j]){
+                    int l=1,r=1;
+                    while(i-l>=0 and j-l>=0 and s[i-l]==t[j-l])++l;
+                    while(i+r<s.length() and j+r<t.length() and s[i+r]==t[j+r])++r;
+                    res+= l*r;
                 }
             }
         }
