5618a773f9c11d057a46dacf7b339bc595ad4b0a
Biswajit Rout <101084372+routb68@users.noreply.github.com>
2022-12-13 23:34:33 +0530

    Time: 241 ms (45.73%), Space: 60.8 MB (18.11%) - LeetHub

--- 0494-target-sum/0494-target-sum.cpp
+++ 0494-target-sum/0494-target-sum.cpp
@@ -1,17 +1,22 @@
 class Solution {
 public:
-    int tar,cnt;
-    void go(vector<int>&v,int idx,int sum){
+    int tar;
+    vector<unordered_map<int,int>>ump;
+    int go(vector<int>&v,int idx,int sum){
         if(idx==v.size()){
-            if(sum==tar)++cnt;
-            return;
+            if(sum==tar)
+            return 1;
+            else return 0;
         }
-        go(v,idx+1,sum+v[idx]);
-        go(v,idx+1,sum-v[idx]);
+        if(ump[idx].find(sum)!=ump[idx].end()) return ump[idx][sum];
+        int pos=go(v,idx+1,sum+v[idx]);
+        int neg=go(v,idx+1,sum-v[idx]);
+        ump[idx][sum]=pos+neg;
+        return pos+neg;
     }
     int findTargetSumWays(vector<int>& nums, int target) {
-        tar=target,cnt=0;
-        go(nums,0,0);
-        return cnt;
+        ump.resize(nums.size()+1);
+        tar=target;
+        return go(nums,0,0);
     }
 };
