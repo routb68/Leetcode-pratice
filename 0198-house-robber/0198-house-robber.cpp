3d5935989574cf4d3ff7ee853b5ed14919657bca
Biswajit Rout <101084372+routb68@users.noreply.github.com>
2022-12-14 14:09:07 +0530

    Time: 5 ms (22.94%), Space: 7.6 MB (78.33%) - LeetHub

---
+++ 0198-house-robber/0198-house-robber.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int rob(vector<int>& nums) {
+        if(nums.size()<2) return nums[0];
+        int dp[nums.size()+1];
+        dp[0]=nums[0],dp[1]=max(nums[1],nums[0]);
+        for(int i=2;i<nums.size();++i){
+            dp[i]=max(dp[i-1],dp[i-2]+nums[i]);
+        }
+        return dp[nums.size()-1];
+    }
+};
