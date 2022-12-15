f9a9f41a6ba2a13bd06aec3deba5c4eb07b674f7
Biswajit Rout <101084372+routb68@users.noreply.github.com>
2022-12-15 07:42:21 +0530

    Time: 518 ms (5.09%), Space: 92.7 MB (12.25%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
@@ -1,18 +1,21 @@
 class Solution {
 public:
-    int dp[1005][1005];
-    int longestCommonSubsequence(string t, string s) {
-        for(int i=1;i<=t.size();++i){
-            for(int j=1;j<=s.size();++j){
-                if(t[i-1]==s[j-1]){
-                    dp[i][j]=1+dp[i-1][j-1];
-                }else{
-                    dp[i][j]=max(dp[i-1][j],dp[i][j-1]);
-                }
-                //cout<<dp[i][j]<<" ";
-            }
-            //cout<<endl;
+    vector<unordered_map<int,int>>ump;
+    int cum(string &t1,string &t2,int a,int b){
+        if(ump[a].find(b)==ump[a].end()){
+            if(a==0  or b==0) return 0;
+            int x;
+        if(t1[a-1]==t2[b-1]) x= 1+ cum(t1,t2,a-1,b-1);
+        else{
+            x= max(cum(t1,t2,a-1,b),cum(t1,t2,a,b-1));
         }
-        return dp[t.size()][s.size()];
+        ump[a][b]=x;
+        }
+        return ump[a][b];
+    }
+    int longestCommonSubsequence(string t1, string t2) {
+        int cnt=0;
+        ump.resize(t1.length()+2);
+        return cum(t1,t2,t1.length(),t2.length());
     }
 };
