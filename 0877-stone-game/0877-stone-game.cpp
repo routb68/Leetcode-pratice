e1b9fdbe056d1b4217db18684ecb8ba2e1166bc1
Biswajit Rout <101084372+routb68@users.noreply.github.com>
2022-12-23 03:18:41 +0530

    Time: 67 ms (14.13%), Space: 9.8 MB (33.46%) - LeetHub

---
+++ 0877-stone-game/0877-stone-game.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int ump[501][501],um[501][501];
+    int com(vector<int>&v,int l,int r,bool p){
+        if(l>r) return 0;
+        if(p){
+            if(ump[l][r]==-1)
+            ump[l][r]=max(v[l]+com(v,l+1,r,0),v[r]+com(v,l,r-1,0));
+            return ump[l][r];
+        }
+        if(um[l][r]==-1)
+        um[l][r]=max(-v[l]+com(v,l+1,r,1),-v[r]+com(v,l,r-1,0));
+        return um[l][r];
+    }
+    bool stoneGame(vector<int>&v) {
+        for(int i=0;i<501;++i)
+            for(int j=0;j<501;++j){
+                ump[i][j]=-1;
+                um[i][j]=-1;
+            }
+        if(v[0]+ com(v,1,v.size()-1,0) >0 or v.back()+com(v,0,v.size()-2,0)>0)
+            return true;
+        return false;
+    }
+};
