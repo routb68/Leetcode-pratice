8477d9bc7e4995e65c40334996b20e1352ba83b8
Biswajit Rout <101084372+routb68@users.noreply.github.com>
2022-12-24 17:43:32 +0530

    Time: 2 ms (78.64%), Space: 7.4 MB (67.92%) - LeetHub

--- 0486-predict-the-winner/0486-predict-the-winner.cpp
+++ 0486-predict-the-winner/0486-predict-the-winner.cpp
@@ -1,23 +1,21 @@
 class Solution {
 public:
     int dp[21][21];
-    int com(vector<int>&v,int l,int r,bool p){
-        if(l>r) return 0;
+    int com(vector<int>&v,int l,int r){
         if(l==r) return v[l];
+        if(l>r) return 0;
         if(dp[l][r]==-1){
-            if(p)
-                dp[l][r]=max(v[l]+com(v,l+1,r,false),v[r]+com(v,l,r-1,false));
-            else
-                dp[l][r]=min(-v[l]+com(v,l+1,r,true),-v[r]+com(v,l,r-1,true));
+            dp[l][r]=max(v[l]+min(com(v,l+2,r),com(v,l+1,r-1)),
+                        v[r]+min(com(v,l,r-2),com(v,l+1,r-1)));
         }
         return dp[l][r];
     }
     bool PredictTheWinner(vector<int>& nums) {
         for(int i=0;i<21;++i){
-            for(int j=0;j<21;++j){
+            for(int j=0;j<21;++j)
                 dp[i][j]=-1;
-            }
         }
-        return com(nums,0,nums.size()-1,true)>=0;
+        int x=com(nums,0,nums.size()-1);
+        return (x>=accumulate(begin(nums),end(nums),0)-x);
     }
 };
