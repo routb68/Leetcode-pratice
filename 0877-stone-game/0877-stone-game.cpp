be07f892c10496261935ec98fa463a1dfaf891aa
Biswajit Rout <101084372+routb68@users.noreply.github.com>
2022-12-24 05:30:21 +0530

    Time: 1558 ms (5.04%), Space: 658.4 MB (5.04%) - LeetHub

--- 0877-stone-game/0877-stone-game.cpp
+++ 0877-stone-game/0877-stone-game.cpp
@@ -1,24 +1,22 @@
 class Solution {
 public:
-    int um[501][501];
-    int com(vector<int>&v,int l,int r,bool p){
+    int dp[501][501];
+    int com(vector<int>v,int l,int r){
+        if(l==r) return v[l];
         if(l>r) return 0;
-        if(p){
-            if(um[l][r]==-1)
-            um[l][r]=max(v[l]+com(v,l+1,r,0),v[r]+com(v,l,r-1,0));
-            return um[l][r];
+        if(dp[l][r]==-1){
+        dp[l][r]= max(v[l]+min(com(v,l+2,r),com(v,l+1,r-1)),
+                  v[r]+min(com(v,l+1,r-1),com(v,l,r-2)));
         }
-        if(um[l][r]==-1)
-        um[l][r]=max(-v[l]+com(v,l+1,r,1),-v[r]+com(v,l,r-1,1));
-        return um[l][r];
+        return dp[l][r];
     }
-    bool stoneGame(vector<int>&v) {
-        for(int i=0;i<501;++i)
-            for(int j=0;j<501;++j){
-                um[i][j]=-1;
-            }
-        if(v[0]+ com(v,1,v.size()-1,0) >0 or v.back()+com(v,0,v.size()-2,0)>0)
-            return true;
-        return false;
+    bool stoneGame(vector<int>& v) {
+        for(int i=0;i<501;++i){
+            for(int j=0;j<501;++j)
+                dp[i][j]=-1;
+        }
+        int sum=accumulate(begin(v),end(v),0);
+        int osum=com(v,0,v.size()-1);
+        return( osum>sum-osum);
     }
 };
