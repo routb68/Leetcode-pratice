7cfbdb7fb82eff9db216aee95a20ae5c6a001dc7
Biswajit Rout <101084372+routb68@users.noreply.github.com>
2022-12-30 04:37:07 +0530

    Time: 1089 ms (64.27%), Space: 129.4 MB (64.82%) - LeetHub

---
+++ 1834-single-threaded-cpu/1834-single-threaded-cpu.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    vector<int> getOrder(vector<vector<int>>& t) {
+        int64_t n=t.size(),time=0,i=0;
+        for(int j=0;j<n;++j) t[j].push_back(j);
+        sort(begin(t),end(t));
+        priority_queue<pair<int,int>,vector<pair<int,int>>,greater<>>pq;
+        vector<int>ans;
+        while(i<n or pq.size()){
+            if(pq.size()==0){
+                time=max(time,(int64_t)t[i][0]);
+            }
+            while(i<n and time>=t[i][0]){
+                pq.emplace(t[i][1],t[i][2]);
+                ++i;
+            }
+            auto [x,y]=pq.top();
+            cout<<x<<" "<<y<<endl;
+            pq.pop();
+            time+=x;
+            ans.push_back(y);
+        }
+        return ans;
+    }
+};
