20701eb7b3412bcc532b547077b15fb278cab7c2
Biswajit Rout <101084372+routb68@users.noreply.github.com>
2023-01-03 13:38:36 +0530

    Time: 0 ms (100.00%), Space: 6.1 MB (12.33%) - LeetHub

--- 2520-count-the-digits-that-divide-a-number/2520-count-the-digits-that-divide-a-number.cpp
+++ 2520-count-the-digits-that-divide-a-number/2520-count-the-digits-that-divide-a-number.cpp
@@ -1,16 +1,15 @@
 class Solution {
 public:
     int countDigits(int num) {
+       int x= num;
         vector<int>v;
-        int x=num;
         while(x){
             v.push_back(x%10);
             x/=10;
         }
-        int cnt=0;
-        for(int &a:v)
-            if(num%a==0)
-                ++cnt;
-        return cnt;
+        for(int &a:v){
+            if(num%a==0)++x;
+        }
+        return x;
     }
 };
