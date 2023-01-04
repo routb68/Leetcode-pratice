634d33ec0aca0de81f7a45c6adfab9000378c8d1
Biswajit Rout <101084372+routb68@users.noreply.github.com>
2023-01-04 11:57:03 +0530

    Time: 7 ms (71.01%), Space: 6 MB (77.75%) - LeetHub

--- 2320-count-number-of-ways-to-place-houses/2320-count-number-of-ways-to-place-houses.cpp
+++ 2320-count-number-of-ways-to-place-houses/2320-count-number-of-ways-to-place-houses.cpp
@@ -1,19 +1,19 @@
 class Solution {
 public:
-    int64_t mod = 1e9+7;
-    int64_t dp[10004][2];
-    int64_t com(int n,int ok){
-        if(n==1) return 1;
-        if(dp[n][ok]!=-1 )return dp[n][ok];
-        if(ok) dp[n][ok]= com(n-1,0);
-        else dp[n][ok]= (com(n-1,0)+com(n-1,1))%mod;
-        return dp[n][ok];
-    }
     int countHousePlacements(int n) {
+        int64_t mod = 1e9+7;
+        int64_t dp[10004][2];
         for(int i=0;i<10004;++i)
             for(int j=0;j<2;++j)
                 dp[i][j]=-1;
-        int64_t ans=(com(n,0)+com(n,1));
+        dp[1][0]=1;dp[1][1]=1;
+        for(int i=2;i<=n;++i){
+            for(int j=0;j<2;++j){
+                if(j==0) dp[i][j]= (dp[i-1][0]+dp[i-1][1])%mod;
+                else dp[i][j]=dp[i-1][0];
+            }
+        }
+        int64_t ans= (dp[n][0]+dp[n][1])%mod;
         return (ans*ans)%mod;
     }
 };
