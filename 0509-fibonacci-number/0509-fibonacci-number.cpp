f290959822da2dcbf4425877cd60f81e1c0c8c4e
Biswajit Rout <101084372+routb68@users.noreply.github.com>
2023-05-07 11:43:32 +0530

    Time: 3 ms (70.23%), Space: 5.9 MB (94.24%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.cpp
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -1,7 +1,11 @@
 class Solution {
 public:
     int fib(int n) {
-        if(n<=1) return n;
-        return fib(n-1)+fib(n-2);
+        int arr[n+2];
+        arr[0]=0,arr[1]=1;
+        for(int i=2;i<=n;++i){
+            arr[i]=arr[i-1]+arr[i-2];
+        }
+        return arr[n];
     }
 };
