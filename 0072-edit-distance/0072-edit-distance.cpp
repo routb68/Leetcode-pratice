ca2cf02bfa60418bd23fe8e3c515f97fa270c853
Biswajit Rout <101084372+routb68@users.noreply.github.com>
2023-05-10 17:14:04 +0530

    Time: 11 ms (78.52%), Space: 7.3 MB (80.83%) - LeetHub

---
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int minDistance(string s1, string s2) {
+        int dp[s1.length()+1][s2.length()+1];
+        for(int i=0;i<=s1.length();++i){
+            dp[i][0]=i;
+        }
+        for(int i=0;i<=s2.length();++i){
+            dp[0][i]=i;
+        }
+        for(int i=1;i<=s1.length();++i){
+            for(int j=1;j<=s2.length();++j){
+                if(s1[i-1]==s2[j-1]){
+                    dp[i][j]=dp[i-1][j-1];
+                }else{
+                    dp[i][j]=1+min({dp[i][j-1],dp[i-1][j],dp[i-1][j-1]});
+                }
+            }
+        }
+        return dp[s1.length()][s2.length()];
+    }
+};
