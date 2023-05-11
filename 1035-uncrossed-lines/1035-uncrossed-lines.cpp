dbd683cb02c320af3fb61b90db7f3982637c3e46
Biswajit Rout <101084372+routb68@users.noreply.github.com>
2023-05-11 11:52:10 +0530

    Time: 21 ms (60.61%), Space: 10.7 MB (80.86%) - LeetHub

--- 1035-uncrossed-lines/1035-uncrossed-lines.cpp
+++ 1035-uncrossed-lines/1035-uncrossed-lines.cpp
@@ -1,24 +1,20 @@
 class Solution {
 public:
-    int dp[505][505];
-    int go (vector<int>&v1,vector<int>&v2,int a,int b){
-        if(dp[a][b]!=-1) return dp[a][b];
-        if(a== 0 or b==0) 
-            return dp[a][b]=0;
-        if(v1[a-1]==v2[b-1]) 
-            return dp[a][b]= 1+ go(v1,v2,a-1,b-1);
-        return dp[a][b]=max({go(v1,v2,a-1,b),
-                    go(v1,v2,a,b-1),
-                    go(v1,v2,a-1,b-1)});
-    }
     int maxUncrossedLines(vector<int>& nums1, vector<int>& nums2) {
-        for(int i=0;i<=nums1.size();++i){
-            for(int j=0;j<=nums2.size();++j){
-                dp[i][j]=-1;
+        int dp[505][505];
+        for(int i=0;i<505;++i) dp[i][0]=0;
+        for(int i=0;i<505;++i) dp[0][i]=0;
+        for(int i=1;i<=nums1.size();++i){
+            for(int j=1;j<=nums2.size();++j){
+                if(nums1[i-1]==nums2[j-1]){
+                    dp[i][j]=1+dp[i-1][j-1];
+                }else{
+                    dp[i][j]= max({dp[i-1][j],
+                                   dp[i][j-1],
+                                   dp[i-1][j-1]});
+                }
             }
         }
-        return go(nums1,nums2,
-                  nums1.size(),
-                  nums2.size());
+        return dp[nums1.size()][nums2.size()];
     }
 };
