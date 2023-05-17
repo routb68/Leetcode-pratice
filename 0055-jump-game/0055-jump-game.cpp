ba86b2da3656875f67c7bfd321979f7180fe8ce3
Biswajit Rout <101084372+routb68@users.noreply.github.com>
2023-05-17 14:08:58 +0530

    Time: 278 ms (21.71%), Space: 48.3 MB (94.57%) - LeetHub

---
+++ 0055-jump-game/0055-jump-game.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool canJump(vector<int>& nums) {
+        bool dp[nums.size()];
+        dp[0]=true;
+        for(int i=1;i<nums.size();++i){
+            dp[i]=false;
+            for(int j=i-1;j>=0;--j){
+                if(nums[j]+j>=i and dp[j]){
+                    dp[i]=true;
+                    break;
+                }
+            }
+        }
+        return dp[nums.size()-1];
+    }
+};
