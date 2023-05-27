464105ce63b8c67f1bfc2f8fc81e3d2b42e2670b
Biswajit Rout <101084372+routb68@users.noreply.github.com>
2023-05-27 11:01:51 +0530

    Time: 25 ms (31.69%), Space: 8.8 MB (36.44%) - LeetHub

--- 0877-stone-game/0877-stone-game.cpp
+++ 0877-stone-game/0877-stone-game.cpp
@@ -1,22 +1,21 @@
 class Solution {
 public:
-    int dp[501][501];
-    int com(vector<int>v,int l,int r){
-        if(l==r) return v[l];
-        if(l>r) return 0;
-        if(dp[l][r]==-1){
-        dp[l][r]= max(v[l]+min(com(v,l+2,r),com(v,l+1,r-1)),
-                  v[r]+min(com(v,l+1,r-1),com(v,l,r-2)));
-        }
-        return dp[l][r];
+    int dp[505][505];
+    int go(vector<int>&v,int i,int j){
+        if(dp[i][j]!=INT_MIN) return dp[i][j];
+        if(i==j-1) return dp[i][j]=max({v[i],v[j]});
+        return dp[i][j]= max(v[i]+min(go(v,i+2,j),go(v,i+1,j-1)),
+                            v[j]+min(go(v,i,j-2),go(v,i+1,j-1)));
     }
-    bool stoneGame(vector<int>& v) {
-        for(int i=0;i<501;++i){
-            for(int j=0;j<501;++j)
-                dp[i][j]=-1;
+    bool stoneGame(vector<int>& ps) {
+        for(int i=0;i<ps.size();++i){
+            for(int j=0;j<ps.size();++j){
+                dp[i][j]=INT_MIN;
+            }
         }
-        int sum=accumulate(begin(v),end(v),0);
-        int osum=com(v,0,v.size()-1);
-        return( osum>sum-osum);
+        int sum = accumulate(begin(ps),end(ps),0);
+        int res = go(ps,0,ps.size()-1);
+        if(sum-res>res) return false;
+        return true;
     }
 };
