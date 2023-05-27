7bffda87b5cae596f1c20d985efc1086e01218d3
Biswajit Rout <101084372+routb68@users.noreply.github.com>
2023-05-27 11:59:16 +0530

    Time: 0 ms (100.00%), Space: 7.5 MB (84.03%) - LeetHub

--- 0877-stone-game/0877-stone-game.cpp
+++ 0877-stone-game/0877-stone-game.cpp
@@ -2,10 +2,10 @@ class Solution {
 public:
     bool stoneGame(vector<int>& ps) {
         int ans =0,res=0;
-        for(int i=0;i<ps.size();++i){
-            if(i%2) ans +=ps[i];
-            else res+=ps[i];
+        for(int i=0;i<ps.size()/2;++i){
+            ans+=ps[i];
         }
+        for(int i=ps.size()/2;i<ps.size();++i) res+=ps[i];
         if(ans==res) return false;
         return true;
     }
