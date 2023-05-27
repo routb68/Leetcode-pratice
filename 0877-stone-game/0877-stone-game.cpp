232b8ba7be6068f398131173ca006de7aac4ef47
Biswajit Rout <101084372+routb68@users.noreply.github.com>
2023-05-27 11:44:43 +0530

    Time: 6 ms (48.99%), Space: 7.5 MB (84.03%) - LeetHub

--- 0877-stone-game/0877-stone-game.cpp
+++ 0877-stone-game/0877-stone-game.cpp
@@ -1,21 +1,12 @@
 class Solution {
 public:
-    int dp[505][505];
-    int go(vector<int>&v,int i,int j){
-        if(dp[i][j]!=INT_MIN) return dp[i][j];
-        if(i==j-1) return dp[i][j]=max({v[i],v[j]});
-        return dp[i][j]= max(v[i]+min(go(v,i+2,j),go(v,i+1,j-1)),
-                            v[j]+min(go(v,i,j-2),go(v,i+1,j-1)));
-    }
     bool stoneGame(vector<int>& ps) {
+        int ans =0,res=0;
         for(int i=0;i<ps.size();++i){
-            for(int j=0;j<ps.size();++j){
-                dp[i][j]=INT_MIN;
-            }
+            if(i%2) ans +=ps[i];
+            else res+=ps[i];
         }
-        int sum = accumulate(begin(ps),end(ps),0);
-        int res = go(ps,0,ps.size()-1);
-        if(sum-res>res) return false;
+        if(ans==res) return false;
         return true;
     }
 };
