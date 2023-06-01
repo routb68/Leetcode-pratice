fad0ead6c628b9158768ebff9f1a2efa401817ac
Biswajit Rout <101084372+routb68@users.noreply.github.com>
2023-06-01 10:48:59 +0530

    Time: 55 ms (42.27%), Space: 70.6 MB (99.68%) - LeetHub

--- 2656-maximum-sum-with-exactly-k-elements/2656-maximum-sum-with-exactly-k-elements.cpp
+++ 2656-maximum-sum-with-exactly-k-elements/2656-maximum-sum-with-exactly-k-elements.cpp
@@ -2,11 +2,6 @@ class Solution {
 public:
     int maximizeSum(vector<int>& nums, int k) {
         int a = *max_element(begin(nums),end(nums));
-        int ans=0;
-        for(int i=0;i<k;++i){
-            ans+=a;
-            ++a;
-        }
-        return ans;
+        return (k*a)+(k*(k-1))/2;
     }
 };
