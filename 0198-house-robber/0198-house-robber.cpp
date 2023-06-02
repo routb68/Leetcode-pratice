e0de466fe2c584230ecdaf3ad05189fde0c30b48
Biswajit Rout <101084372+routb68@users.noreply.github.com>
2023-06-02 20:55:39 +0530

    Time: 0 ms (100.00%), Space: 7.7 MB (51.97%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -1,14 +1,12 @@
 class Solution {
 public:
-    int dp[105];
-    int go(vector<int>&v,int idx){
-        if(dp[idx]!=-1) return dp[idx];
-        if(idx==1) return dp[idx]=v[0];
-        if(idx==2) return dp[idx]=max(v[0],v[1]);
-        return dp[idx]=max(go(v,idx-1),go(v,idx-2)+v[idx-1]);
-    }
     int rob(vector<int>& nums) {
-        for(int i=0;i<105;++i)dp[i]=-1;
-        return go(nums,nums.size());
+        if(nums.size()<2) return nums[0];
+        int a=nums[0],b=max(nums[0],nums[1]),ans=b;
+        for(int i=2;i<nums.size();++i){
+            int temp = max(b,a+nums[i]);
+            a=b,b=temp,ans=temp;
+        }
+        return ans;
     }
 };
