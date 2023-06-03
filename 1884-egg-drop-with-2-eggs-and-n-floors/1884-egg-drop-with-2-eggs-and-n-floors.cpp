fcc23a313d37496dcc4bab5c63a4ae401653802f
Biswajit Rout <101084372+routb68@users.noreply.github.com>
2023-06-03 11:45:08 +0530

    Time: 65 ms (42.93%), Space: 5.9 MB (78.29%) - LeetHub

--- 1884-egg-drop-with-2-eggs-and-n-floors/1884-egg-drop-with-2-eggs-and-n-floors.cpp
+++ 1884-egg-drop-with-2-eggs-and-n-floors/1884-egg-drop-with-2-eggs-and-n-floors.cpp
@@ -1,10 +1,24 @@
 class Solution {
 public:
     int twoEggDrop(int n) {
-        int res=0;
-        while((res*(res+1))<n+n){
-            ++res;
+        int f=n,egg=2;
+        int dp[egg+1][f+1];
+        for(int i=1;i<=egg;++i){
+            dp[i][0]=0;
+            dp[i][1]=1;
         }
-        return res;
+        for(int i=0;i<=f;++i){
+            dp[1][i]=i;
+        }
+        for(int i =2;i<=f;++i){
+            for(int j=2;j<=egg;++j){
+                dp[j][i]=INT_MAX;
+                for(int x=1;x<=i;++x){
+                    dp[j][i]=min(dp[j][i],1+max(dp[j-1][x-1],dp[j][i-x]));
+                }
+            }
+        }
+        return dp[egg][f];
+        
     }
 };
