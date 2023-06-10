9f85d73ec7613582a9802a775d5a913274f463a1
Biswajit Rout <101084372+routb68@users.noreply.github.com>
2023-06-10 16:01:10 +0530

    Time: 535 ms (5.04%), Space: 80.3 MB (16.11%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.cpp
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -1,36 +1,18 @@
 class Solution {
 public:
-	int dp[505][505];
-	int go(string s1, string s2, int n, int m) {
-		int res;
-		if (dp[n][m] == -1) {
-            if(n==0) res=m;
-            else if (m==0) res=n;
-            else if (s1[n - 1] == s2[m - 1])
-				res = go(s1, s2, n - 1, m - 1);
-			else
-				res = 1 + min({go(s1, s2, n - 1, m),go(s1, s2, n - 1, m - 1),
-                              go(s1, s2, n, m - 1)});
-		}else{
-            return dp[n][m];
-        }
-		return dp[n][m] = res;
-	}
-	int minDistance(string word1, string word2) {
-		for (int i = 0; i < 505; ++i) {
-			for (int j = 0; j < 505; ++j) {
-				dp[i][j] = -1;
-			}
-		}
-        for(int i=0;i<505;++i) dp[i][0]=i;
-        for(int i=0;i<505;++i) dp[0][i]=i;
-        int ans =go(word1, word2, word1.length(), word2.length());
-        for(int i=0;i<=word1.length();++i){
-            for(int j=0;j<=word2.length();++j){
-                cout<<dp[i][j]<<" ";
+    int dp[505][505];
+    int go(string s1,string s2,int m,int n){
+        if(dp[m][n]!=-1) return dp[m][n];
+        if(m==0 or n==0) return dp[m][n]=max(m,n);
+        if(s1[m-1]==s2[n-1]) return dp[m][n]=go(s1,s2,m-1,n-1);
+        return dp[m][n]=1+min({go(s1,s2,m-1,n),go(s1,s2,m,n-1),go(s1,s2,m-1,n-1)});
+    }
+    int minDistance(string word1, string word2) {
+        for(int i=0;i<505;++i){
+            for(int j=0;j<505;++j){
+                dp[i][j]=-1;
             }
-            cout<<endl;
         }
-		return ans;
-	}
+        return go (word1,word2,word1.size(),word2.size());
+    }
 };
