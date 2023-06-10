24122bb897128ee8de47f746601aa4a5099c7e19
Biswajit Rout <101084372+routb68@users.noreply.github.com>
2023-06-10 11:43:18 +0530

    Time: 96 ms (74.39%), Space: 9.9 MB (97.92%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -1,15 +1,16 @@
 class Solution {
 public:
     int coinChange(vector<int>& cs, int amt) {
+        // sort(begin(cs),end(cs));
+        int n=cs.size();
         int dp[amt+1];
         dp[0]=0;
         for(int i=1;i<=amt;++i){
             dp[i]=INT_MAX;
-            for(int j=0;j<cs.size();++j){
-                if(cs[j]<=i){
-                    int sub_res=dp[i-cs[j]];
-                    if(sub_res!=INT_MAX){
-                        dp[i]=min(dp[i],sub_res+1);
+            for(int j=0;j<n;++j){
+                if(i>=cs[j]){
+                    if(dp[i-cs[j]]!=INT_MAX){
+                        dp[i]=min(dp[i],dp[i-cs[j]]+1);
                     }
                 }
             }
