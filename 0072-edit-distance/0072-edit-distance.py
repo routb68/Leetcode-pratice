6b15a7199530cede950b73d8e9f2c8d81f3760bd
Biswajit Rout <101084372+routb68@users.noreply.github.com>
2023-06-11 12:04:21 +0530

    Time: 170 ms (44.67%), Space: 20.1 MB (35.26%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.py
+++ 0072-edit-distance/0072-edit-distance.py
@@ -1,19 +1,18 @@
 class Solution:
-    
     def minDistance(self, s1: str, s2: str) -> int:
-        dp = {}
-        def go(m:int,n:int)-> int:
-            if (m,n) in dp:
-                return dp[(m,n)]
-            if(m==0 or n==0):
-                return max(n,m)
-                # return dp[m][n]
-            if(s1[m-1]==s2[n-1]):
-                ans=go(m-1,n-1)
-                # return dp[m][n]
-            else :
-                ans=1+min(go(m,n-1),go(m-1,n),go(m-1,n-1))
-            dp[(m,n)]=ans
-            return ans
-            # return dp[m][n]
-        return go(len(s1),len(s2))
+        m = len(s1)
+        n = len(s2)
+        dp = [ [ 0 for x in range(m+1)] for y in range (n+1)]
+        for x in range (m+1):
+            dp[0][x]=x
+        for y in range (n+1):
+            dp[y][0]=y
+        for y in range (1,n+1):
+            for x in range (1,m+1):
+                if(s1[x-1]==s2[y-1]):
+                    dp[y][x] = dp[y-1][x-1]
+                else :
+                    dp[y][x]= 1 +min(dp[y-1][x],dp[y-1][x-1],dp[y][x-1])
+                # print(dp[y][x],end=" ")
+            # print()
+        return dp[n][m]
