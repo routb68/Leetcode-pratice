28b815bdf60197561f37e9c93686766da107a086
Biswajit Rout <101084372+routb68@users.noreply.github.com>
2023-06-11 13:12:31 +0530

    Time: 10 ms (85.92%), Space: 10.3 MB (85.31%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -1,16 +1,29 @@
 class Solution {
 public:
-    int lengthOfLIS(vector<int>& nums) {
-        int dp[nums.size()];
-        dp[0]=1;
-        for(int i=1;i<nums.size();++i){
-            dp[i]=1;
-            for(int j=0;j<i;++j){
-                if(nums[j]<nums[i]){
-                    dp[i]=max(dp[i],1+dp[j]);
-                }
+    int cellidx(int *dp,int low,int hi,int ele){
+        while(low<hi){
+            int m = low + (hi-low)/2;
+            if(dp[m]>=ele){
+                hi=m;
+            }else{
+                low =m+1;
             }
         }
-        return *max_element(dp,dp+nums.size());
+        return hi;
+    }
+    int lengthOfLIS(vector<int>& nums) {
+        int dp[nums.size()];
+        dp[0]=nums[0];
+        int len =1;
+        for(int i=1;i<nums.size();++i){
+            if(nums[i]>dp[len-1]){
+                dp[len]=nums[i];
+                ++len;
+            }else{
+                int idx = cellidx(dp,0,len-1,nums[i]);
+                dp[idx]=nums[i];
+            }
+        }
+        return len;
     }
 };
