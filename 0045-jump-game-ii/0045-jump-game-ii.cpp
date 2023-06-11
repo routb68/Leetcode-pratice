02ec6705646a27cd0ad112d65e0b979bf6a7aac6
Biswajit Rout <101084372+routb68@users.noreply.github.com>
2023-06-11 13:51:46 +0530

    Time: 1869 ms (5.00%), Space: 16.6 MB (43.55%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.cpp
+++ 0045-jump-game-ii/0045-jump-game-ii.cpp
@@ -3,16 +3,16 @@ public:
     int jump(vector<int>& nums) {
         int dp[nums.size()];
         dp[0]=0;
-        for(int i=1;i<nums.size();++i){
+        for (int i=1;i<nums.size();++i){
             dp[i]=INT_MAX;
             for(int j=0;j<i;++j){
-                if(nums[j]+j>=i){
-                    if(dp[j]!=INT_MAX){
-                        dp[i]=min(dp[i],dp[j]+1);
-                    }
+                if(dp[j]!=INT_MAX and nums[j]+j>=i){
+                    dp[i]=min(dp[i],dp[j]+1);
                 }
             }
+            // cout<<dp[i]<<" ";
         }
+        if(dp[nums.size()-1]==INT_MAX) return -1;
         return dp[nums.size()-1];
     }
 };
