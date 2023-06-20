9ebc870284289bb603e68d8080aa1ccec2df6e4a
Biswajit Rout <101084372+routb68@users.noreply.github.com>
2023-06-20 23:39:14 +0530

    Time: 58 ms (6.88%), Space: 16.2 MB (82.98%) - LeetHub

--- 1108-defanging-an-ip-address/1108-defanging-an-ip-address.py
+++ 1108-defanging-an-ip-address/1108-defanging-an-ip-address.py
@@ -1,14 +1,3 @@
-class Solution {
-public:
-    string defangIPaddr(string a) {
-        string s;
-        for(char &x:a){
-            if(x=='.'){
-                s+="[.]";
-            }else{
-                s+=x;
-            }
-        }
-        return s;
-    }
-};
+class Solution:
+    def defangIPaddr(self, address: str) -> str:
+        return address.replace('.', '[.]')
