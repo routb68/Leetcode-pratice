7d33e393763705de78580188dc60b708956dda19
Biswajit Rout <101084372+routb68@users.noreply.github.com>
2023-06-24 19:29:37 +0530

    Time: 2990 ms (5.03%), Space: 389.7 MB (5.02%) - LeetHub

--- 1027-longest-arithmetic-subsequence/1027-longest-arithmetic-subsequence.cpp
+++ 1027-longest-arithmetic-subsequence/1027-longest-arithmetic-subsequence.cpp
@@ -1,27 +1,31 @@
 class Solution {
 public:
-    int go(vector<int>&nums,int d){
-        unordered_map<int,int>dp;
-        dp[nums[0]] =1;
+    int go(vector<int>&nums,int diff){
+        unordered_map<int,int>ump;
+        ump[nums[0]]=1;
         int ans =1;
         for(int i=1;i<nums.size();++i){
-            if (dp.find(nums[i] - d) != dp.end()) {
-                dp[nums[i]] = max(dp[nums[i] - d] + 1, (dp.find(nums[i]) == dp.end() ? 0 : dp[nums[i]]));
-            } else {
-                dp[nums[i]] = 1;
+            if(ump.find(nums[i]-diff)==ump.end()){
+                ump[nums[i]]=1;
+            }else{
+                int x =0;
+                if(ump.find(nums[i])!=ump.end()){
+                    x = ump[nums[i]];
+                }
+                ump[nums[i]] = max(ump[nums[i]-diff]+1,x);
             }
-            ans = max(ans, dp[nums[i]]);
+            ans = max(ans,ump[nums[i]]);
         }
-        
-               return ans;
+        return ans;
     }
     int longestArithSeqLength(vector<int>& nums) {
         int diff = *max_element(begin(nums),end(nums)) - 
             *min_element(begin(nums),end(nums));
-        int res = 0;
-        for(int i=-diff;i<=diff;++i){
+        int res = 0 ;
+        for(int i =-diff;i<=diff;++i){
             res = max(res,go(nums,i));
         }
+        // cout<<endl;
         return res;
     }
 };
