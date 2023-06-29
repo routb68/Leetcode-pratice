4182e15886f4d1d16a5d40919172e8f94c351425
Biswajit Rout <101084372+routb68@users.noreply.github.com>
2023-06-29 11:50:33 +0530

    Time: 77 ms (79.15%), Space: 10.5 MB (80.57%) - LeetHub

--- 2400-number-of-ways-to-reach-a-position-after-exactly-k-steps/2400-number-of-ways-to-reach-a-position-after-exactly-k-steps.cpp
+++ 2400-number-of-ways-to-reach-a-position-after-exactly-k-steps/2400-number-of-ways-to-reach-a-position-after-exactly-k-steps.cpp
@@ -1,20 +1,21 @@
 class Solution {
 public:
-    unordered_map<int,long long>ump[1001];
-    const long long mod = 1e9+7;
-    long long go(int a,int b,int c){
-        if(ump[c].find(a)!=ump[c].end())
-            return ump[c][a];
-        if(c==0){
-            if(a==b)return 1;
+    int dp[1001][1001];
+    const int mod =1e9+7;
+    int go(int k,int dist){
+        if(k<=dist){
+            if(k==dist) return 1;
             return 0;
         }
-        ump[c][a] = (go(a+1,b,c-1)+go(a-1,b,c-1))%mod;
-        return ump[c][a];
+        if(dp[k][dist]==0){
+            dp[k][dist] = (1+ go(k-1,dist+1)+go(k-1,abs(dist-1)))%mod;
+        }
+        return dp[k][dist]-1;
     }
     int numberOfWays(int startPos, int endPos, int k) {
-        // cout<<ump[0][0]<<endl;
-        return (go(startPos+1,endPos,k-1) + go(startPos-1,endPos,k-1))%mod;
-        // return 1;
+        for(int i=0;i<1001;++i)
+            for (int j=0;j<1001;++j)
+                dp[i][j]=0;
+        return go(k,abs(startPos-endPos));
     }
 };
