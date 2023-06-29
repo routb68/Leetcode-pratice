9d0acfe15835fe68ca2ec48af1a451d8728878d9
Biswajit Rout <101084372+routb68@users.noreply.github.com>
2023-06-29 13:04:32 +0530

    Time: 3052 ms (15.52%), Space: 30.1 MB (94.83%) - LeetHub

--- 2183-count-array-pairs-divisible-by-k/2183-count-array-pairs-divisible-by-k.py
+++ 2183-count-array-pairs-divisible-by-k/2183-count-array-pairs-divisible-by-k.py
@@ -5,14 +5,12 @@ class Solution:
         ans = 0
         for i in range(len(nums)):
             currgcd = gcd(nums[i],k)
-            # print(currgcd)
             for x in ump:
                 x1 = int(x)
                 if ( x1* currgcd) %k==0:
                     ans+= ump[x]
             if currgcd in ump:
-                val = ump[currgcd]
-                ump[currgcd] = val+1
+                ump[currgcd]+=1
             else:
                 ump[currgcd] =1
         return ans
