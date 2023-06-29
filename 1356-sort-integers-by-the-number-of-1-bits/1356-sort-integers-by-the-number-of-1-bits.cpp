e4b65c8b9d096aaf87e4a79558d3599de6daaa1a
Biswajit Rout <101084372+routb68@users.noreply.github.com>
2023-06-29 18:33:37 +0530

    Time: 13 ms (46.76%), Space: 10.7 MB (36.22%) - LeetHub

--- 1356-sort-integers-by-the-number-of-1-bits/1356-sort-integers-by-the-number-of-1-bits.cpp
+++ 1356-sort-integers-by-the-number-of-1-bits/1356-sort-integers-by-the-number-of-1-bits.cpp
@@ -5,7 +5,7 @@ public:
         for(int i=0;i<nums.size();++i){
             int x =nums[i];
             int cnt =0;
-            for(int j=0;j<30;++j){
+            for(int j=0;j<19;++j){
                 if( x & (1<<j))++cnt;
             }
             v.push_back(make_pair(cnt,x));
