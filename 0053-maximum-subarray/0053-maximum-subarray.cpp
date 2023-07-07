f157d4beab6043956939fa15bff1694aa5fd0d0c
Biswajit Rout <101084372+routb68@users.noreply.github.com>
2023-07-07 10:40:35 +0530

    Time: 126 ms (50.60%), Space: 67.7 MB (37.24%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.cpp
+++ 0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -1,10 +1,10 @@
 class Solution {
 public:
     int maxSubArray(vector<int>& nums) {
-        int ans=INT_MIN,psum=0;
+        int ans =INT_MIN,psum=0;
         for(int &x:nums){
             psum+=x;
-            ans=max(psum,ans);
+            ans=max(ans,psum);
             psum=max(0,psum);
         }
         return ans;
