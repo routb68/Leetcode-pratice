f00ec126710c4673006d4bdb3de68ad3d57ed48b
Biswajit Rout <101084372+routb68@users.noreply.github.com>
2023-07-11 13:19:20 +0530

    Time: 134 ms (55.50%), Space: 16.9 MB (36.30%) - LeetHub

--- 2418-sort-the-people/2418-sort-the-people.py
+++ 2418-sort-the-people/2418-sort-the-people.py
@@ -3,7 +3,9 @@ class Solution:
         temp = []
         for i in range(len(names)):
             temp.append([heights[i],names[i]])
-        temp.sort(reverse=True)
-        for i in range(len(names)):
-            names[i]= temp[i][1]
+        temp.sort()
+        idx =0
+        for i in reversed(range(len(names))):
+            names[idx]= temp[i][1]
+            idx+=1
         return names
