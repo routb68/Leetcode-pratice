0b96943d1c382bd666883e649d8adee65ab65739
Biswajit Rout <101084372+routb68@users.noreply.github.com>
2023-07-11 12:52:53 +0530

    Time: 0 ms (100.00%), Space: 6.3 MB (45.58%) - LeetHub

---
+++ 0791-custom-sort-string/0791-custom-sort-string.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    string customSortString(string order, string s) {
+        unordered_map<char,int>ump;
+        for(char&x:s)
+            ++ump[x];
+        vector<int>v(26,0);
+        string ans ="";
+        for(char &x:order){
+            if(ump.find(x)!=ump.end()){
+                int freq = ump[x];
+                while(ump[x]){
+                    ans+=x;
+                    --ump[x];
+                }
+                ump.erase(x);
+            }
+        }
+        for(auto &[x,y]:ump){
+            while(y--){
+                ans+=x;
+            }
+        }
+        return ans;
+    }
+};
