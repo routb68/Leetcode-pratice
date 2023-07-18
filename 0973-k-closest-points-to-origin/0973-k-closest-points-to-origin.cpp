cbc977bace72083f566aad7541a286f0da758dc9
Biswajit Rout <101084372+routb68@users.noreply.github.com>
2023-07-18 10:42:56 +0530

    Time: 225 ms (84.56%), Space: 67.2 MB (63.09%) - LeetHub

--- 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.cpp
+++ 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.cpp
@@ -2,11 +2,12 @@ class Solution {
 public:
     vector<vector<int>> kClosest(vector<vector<int>>& pts, int k) {
         vector<vector<int>> ans;
-        priority_queue<pair<int,int>>pq;
+        priority_queue<pair<long long ,int>,vector<pair<long long,int>>,
+        greater<pair<long long,int>>>pq;
         for(int i=0;i<pts.size();++i){
-            int x = pts[i][0],y = pts[i][1];
+            long long x = pts[i][0],y = pts[i][1];
             // auto z = make_pair();
-            pq.push(make_pair(-((x*x)+(y*y)),i));
+            pq.push(make_pair(((x*x)+(y*y)),i));
         }
         while(k--){
             int idx = pq.top().second;pq.pop();
