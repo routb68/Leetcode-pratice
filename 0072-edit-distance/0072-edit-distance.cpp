2740c2d1e3f709de64682b09630226096f0d7212
Biswajit Rout <101084372+routb68@users.noreply.github.com>
2023-07-20 10:08:20 +0530

    Time: 14 ms (64.39%), Space: 7.2 MB (85.44%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.cpp
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -1,23 +1,18 @@
 class Solution {
 public:
-    int us[5001][501];
-    int com(string s1,string s2,int m,int n){
-        if(us[m][n]!=-1)
-            return us[m][n];
-        if(n==0)
-            return us[m][n]=m;
-        if(m==0)
-            return us[m][n]=n;
-        if(s1[m-1]==s2[n-1]){
-            return us[m][n]=com(s1,s2,m-1,n-1);
-        }else{
-            return us[m][n]= 1 + min({com(s1,s2,m-1,n),com(s1,s2,m-1,n-1),com(s1,s2,m,n-1)});
-        }
-    }
-    int minDistance(string word1, string word2) {
-        for(int i=0;i<501;++i)
-            for(int j=0;j<501;++j)
-                us[i][j]=-1;
-        return com(word1,word2,word1.length(),word2.length());
+    int minDistance(string s1, string s2) {
+        int m=s1.size(),n=s2.size();
+        int dp[m+1][n+1];
+        for(int i=0;i<=m;++i) dp[i][0]=i;
+        for(int i=0;i<=n;++i) dp[0][i]=i;
+        for(int i=1;i<=m;++i)
+            for(int j = 1;j<=n;++j){
+                if(s1[i-1]==s2[j-1]){
+                    dp[i][j]=dp[i-1][j-1];
+                }else{
+                    dp[i][j]= 1+min({dp[i-1][j],dp[i-1][j-1],dp[i][j-1]});
+                }
+            }
+        return dp[m][n];
     }
 };
