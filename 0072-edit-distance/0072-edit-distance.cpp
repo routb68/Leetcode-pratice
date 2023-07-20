2ea171959a70f244c0e2361532de01ad81058536
Biswajit Rout <101084372+routb68@users.noreply.github.com>
2023-07-20 10:02:46 +0530

    Time: 1165 ms (5.02%), Space: 90.4 MB (5.14%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.cpp
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -1,20 +1,23 @@
 class Solution {
 public:
-    map<pair<int,int>,int>us;
+    int us[5001][501];
     int com(string s1,string s2,int m,int n){
-        if(us.find(make_pair(m,n))!=us.end())
-            return us[make_pair(m,n)];
+        if(us[m][n]!=-1)
+            return us[m][n];
         if(n==0)
-            return us[make_pair(m,n)]=m;
+            return us[m][n]=m;
         if(m==0)
-            return us[make_pair(m,n)]=n;
+            return us[m][n]=n;
         if(s1[m-1]==s2[n-1]){
-            return us[make_pair(m,n)]=com(s1,s2,m-1,n-1);
+            return us[m][n]=com(s1,s2,m-1,n-1);
         }else{
-            return us[make_pair(m,n)]= 1 + min({com(s1,s2,m-1,n),com(s1,s2,m-1,n-1),com(s1,s2,m,n-1)});
+            return us[m][n]= 1 + min({com(s1,s2,m-1,n),com(s1,s2,m-1,n-1),com(s1,s2,m,n-1)});
         }
     }
     int minDistance(string word1, string word2) {
+        for(int i=0;i<501;++i)
+            for(int j=0;j<501;++j)
+                us[i][j]=-1;
         return com(word1,word2,word1.length(),word2.length());
     }
 };
