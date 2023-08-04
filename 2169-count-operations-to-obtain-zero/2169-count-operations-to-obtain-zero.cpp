f6f8f71a33fbce5a9fa5b57fb619c5e74ed9cc2d
Biswajit Rout <101084372+routb68@users.noreply.github.com>
2023-08-04 22:46:01 +0530

    Time: 0 ms (100.00%), Space: 6 MB (13.61%) - LeetHub

--- 2169-count-operations-to-obtain-zero/2169-count-operations-to-obtain-zero.cpp
+++ 2169-count-operations-to-obtain-zero/2169-count-operations-to-obtain-zero.cpp
@@ -1,13 +1,15 @@
 class Solution {
 public:
-    int countOperations(int num1, int num2) {
-        int cnt =0;
-        if(num1<num2)swap(num1,num2);
-        while(num2 !=0){
-            num1-=num2;
-            ++cnt;
-            if(num1<num2)swap(num1,num2);
-        }
-        return cnt;
+    int countOperations(int a, int b) {
+        int res = 0;
+    while (min(a, b) > 0) {
+        if (a > b)
+            swap(a, b);
+        res += b / a;
+        b %= a;
+    }
+    return res;
     }
 };
+
+
