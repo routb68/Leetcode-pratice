ba71cadcdcc047dc085b3c6df81bf5e62b5ea560
Biswajit Rout <101084372+routb68@users.noreply.github.com>
2023-08-06 12:17:45 +0530

    Time: 8 ms (66.67%), Space: 17.5 MB (66.67%) - LeetHub

--- 2811-check-if-it-is-possible-to-split-array/2811-check-if-it-is-possible-to-split-array.cpp
+++ 2811-check-if-it-is-possible-to-split-array/2811-check-if-it-is-possible-to-split-array.cpp
@@ -1,34 +1,10 @@
 class Solution {
 public:
-    int pre[105];
-    map<pair<int,int>,bool>us;
-    bool com(vector<int>&nums,int m,int l,int hi){
-        if(us.find(make_pair(l,hi))!=us.end()){
-            return us[make_pair(l,hi)];
-        }
-        if(hi-l==1){
-            if(nums[l]+nums[hi]>=m) return true;
-            return false;
-        }
-        int x = pre[hi]-pre[l];
-        bool ok = false;
-        if(x>=m){
-           ok = (ok or com(nums,m,l+1,hi));
-        }
-        x = pre[hi-1]-pre[l]+nums[l];
-        if(x>=m){
-            ok = (ok or com(nums,m,l,hi-1));
-        }
-        us[make_pair(l,hi)]=ok;
-        return ok;
-        
-    }
     bool canSplitArray(vector<int>& nums, int m) {
-        if(nums.size()<3) return true;
-        pre[0]=nums[0];
+        if(nums.size()<=2) return true;
         for(int i=1;i<nums.size();++i){
-            pre[i]=pre[i-1]+nums[i];
+            if(nums[i-1]+nums[i]>=m) return true;
         }
-        return com(nums,m,0,nums.size()-1);
+        return false;
     }
 };
