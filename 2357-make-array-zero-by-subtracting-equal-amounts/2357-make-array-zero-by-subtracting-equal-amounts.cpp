ec916dff2c9780e877c17dfc943e72ceccbdcb1a
Biswajit Rout <101084372+routb68@users.noreply.github.com>
2023-08-08 10:34:32 +0530

    Time: 0 ms (100.00%), Space: 8.3 MB (46.56%) - LeetHub

--- 2357-make-array-zero-by-subtracting-equal-amounts/2357-make-array-zero-by-subtracting-equal-amounts.cpp
+++ 2357-make-array-zero-by-subtracting-equal-amounts/2357-make-array-zero-by-subtracting-equal-amounts.cpp
@@ -1,15 +1,20 @@
 class Solution {
 public:
     int minimumOperations(vector<int>& nums) {
-        priority_queue<int,vector<int>,greater<int>>pq(begin(nums),end(nums));
-        int sum =0,cnt =0;
-        while(pq.size()){
-            if(pq.top()>sum){
-                sum += (pq.top()-sum);
-                ++cnt;
+        priority_queue<int,vector<int>,greater<int>>pq;
+        for(int i=0;i<nums.size();++i){
+            if(nums[i]){
+                pq.push(nums[i]);
             }
-            pq.pop();
         }
-        return cnt;
+        int ans =0,temp=0;
+        while(pq.size()){
+            int x = pq.top();pq.pop();
+            if(x>temp){
+                temp=x;
+                ++ans;
+            }
+        }
+        return ans;
     }
 };
