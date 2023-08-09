d98d47d279297c48aa9be899be2dea68ae59423b
Biswajit Rout <101084372+routb68@users.noreply.github.com>
2023-08-09 09:53:13 +0530

    Time: 7 ms (95.72%), Space: 10.8 MB (31.30%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -2,15 +2,12 @@ class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int tar) {
         unordered_map<int,int>ump;
-        vector<int>v;
         for(int i=0;i<nums.size();++i){
             if(ump.find(tar-nums[i])!=ump.end()){
-                v.push_back(i);
-                v.push_back(ump[tar-nums[i]]);
-                return v;
+                return {i,ump[tar-nums[i]]};
             }
             ump[nums[i]]=i;
-        }
-        return v;
+        }   
+        return {};
     }
 };
