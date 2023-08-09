a4177b485a3aa283a1020b61078c11247b040029
Biswajit Rout <101084372+routb68@users.noreply.github.com>
2023-08-09 11:02:41 +0530

    Time: 4 ms (72.09%), Space: 7.1 MB (99.07%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.cpp
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -1,14 +1,14 @@
 class Solution {
 public:
     bool isPalindrome(string s) {
-        int i =0,j= s.length()-1;
+        int i =0,j = s.size()-1;
         while(i<j){
-            while(isalnum(s[i])==false and i<j)++i;
-            while(isalnum(s[j])==false and i<j)--j;
+            while(i< s.size() and iswalnum(s[i])==false)++i;
+            while(j>=0 and iswalnum(s[j])==false)--j;
+            if(j<=i) continue;
             if(tolower(s[i])!=tolower(s[j]))
                 return false;
-            ++i;
-            --j;
+            ++i;--j;
         }
         return true;
     }
