d60a629a5be7fe21c9edb0dfd78bb56f6dd45f13
Biswajit Rout <101084372+routb68@users.noreply.github.com>
2023-08-22 13:04:57 +0530

    Time: 26 ms (76.63%), Space: 12.1 MB (61.27%) - LeetHub

--- 2712-minimum-cost-to-make-all-characters-equal/2712-minimum-cost-to-make-all-characters-equal.cpp
+++ 2712-minimum-cost-to-make-all-characters-equal/2712-minimum-cost-to-make-all-characters-equal.cpp
@@ -1,30 +1,13 @@
 class Solution {
 public:
-	long long minimumCost(string s) {
-		long long pre[s.length()], suf[s.length()];
-		for (int i = 0; i < s.length(); ++i) {
-			pre[i] = 0;
-			if (i > 0 and s[i] != s[i - 1])
-				pre[i] = i ;
-			if (i != 0) pre[i] += pre[i - 1];
-			// cout << pre[i] << " ";
-		}
-		// cout << endl;
-		for (int i = s.length() - 1; i >= 0; --i) {
-			suf[i] = 0;
-			if (i != s.length() - 1 and s[i] != s[i + 1])
-				suf[i] = s.length() - i-1;
-            if(i!=s.length()-1)suf[i]+=suf[i+1];
-		}
-        // for(int i=0;i<s.length();++i){
-        //     cout << suf[i] << " ";
-        // }
-        // cout<<endl;
-		long long ans = 1e18;
-		for (int i = 0; i < s.length(); ++i) {
-			long long x = pre[i] + suf[i];
-			ans = min(ans, x);
-		}
-		return ans;
-	}
+    long long minimumCost(string nums) {
+        long long ans =0;
+        for(int i=1;i<nums.size();++i){
+            if(nums[i]!=nums[i-1]){
+                int x = nums.size()-i;
+                ans += min({i,x});
+            }
+        }
+        return ans;
+    }
 };
