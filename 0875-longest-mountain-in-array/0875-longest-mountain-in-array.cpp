1a48a94ff2ad19615eae21675be48e0113b43366
Biswajit Rout <101084372+routb68@users.noreply.github.com>
2023-08-29 01:50:32 +0530

    Time: 12 ms (88.54%), Space: 18.4 MB (80.4%) - LeetHub

---
+++ 0875-longest-mountain-in-array/0875-longest-mountain-in-array.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    int longestMountain(vector<int>& arr) {
+        int ans =0;
+        int pre =0;
+        int cnt=0;
+        bool up = false,dow = false;
+        for( int i=1;i<arr.size();++i){
+            if(arr[i]<arr[i-1]){
+                if(up==true){
+                    up = false;
+                    dow = true;
+                    ++cnt;
+                }else if(dow==true){
+                    ++cnt;
+                }else{
+                     if(dow==true)ans = max(ans,cnt);
+                    cnt=0;
+                    up = false;
+                    dow=false;
+                }
+            }else if (arr[i]>arr[i-1]){
+                if(up==true){
+                    ++cnt;
+                }else{
+                     if(dow==true)ans = max(ans,cnt);
+                    dow = false;
+                    up = true;
+                    cnt = 2;
+                }
+            }else{
+                 if(dow==true)ans = max(ans,cnt);
+                    cnt=0;
+                    up = false;
+                    dow=false;
+            }
+        }
+        if(dow==true)ans= max(ans,cnt);
+        return ans;
+    }
+};
