a5192579eb9504887be44660ba0724a6adea2c3c
Biswajit Rout <101084372+routb68@users.noreply.github.com>
2023-09-03 11:41:03 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (100.00%) - LeetHub

---
+++ 2839-check-if-strings-can-be-made-equal-with-operations-i/2839-check-if-strings-can-be-made-equal-with-operations-i.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool canBeEqual(string s1, string s2) {
+        int arr1[26],brr1[26],arr2[26],brr2[26];
+        for( int i=0;i<26;++i){
+            arr1[i]=0;arr2[i]=0;brr1[i]=0;brr2[i]=0;
+        }
+        for( int i=0;i<s1.size();++i){
+            if(i&1){
+                ++arr1[s1[i]-'a'];
+                ++arr2[s2[i]-'a'];
+            }else{
+                ++brr1[s1[i]-'a'];
+                ++brr2[s2[i]-'a'];
+            }
+        }
+        for(int i=0;i<26;++i){
+            if(arr1[i]!=arr2[i] or brr1[i]!=brr2[i])
+                return false;
+        }
+        return true;
+    }
+};
