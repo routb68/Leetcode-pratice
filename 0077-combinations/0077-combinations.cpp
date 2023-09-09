fe38b52afee23b13fec59ba77b86ffaee32f1060
Biswajit Rout <101084372+routb68@users.noreply.github.com>
2023-09-09 20:04:16 +0530

    Time: 151 ms (15.80%), Space: 132.3 MB (12.84%) - LeetHub

--- 0077-combinations/0077-combinations.cpp
+++ 0077-combinations/0077-combinations.cpp
@@ -1,21 +1,20 @@
 class Solution {
 public:
-    vector<vector<int>>ans;
-    void com(int n,int k, vector<int>&v){
-        if(n==0){
-            if(v.size()==k)
-                ans.push_back(v);
-            return;
-        }
+    vector<vector<int>> ans;
+    void go(int n,int k,vector<int>&v){
+        if(n==0)
+        {if(k==0){
+            ans.push_back(v);
+        }return;}
         v.push_back(n);
-        com(n-1,k,v);
+        go(n-1,k-1,v);
         v.pop_back();
-        com(n-1,k,v);
+        go(n-1,k,v);
     }
     vector<vector<int>> combine(int n, int k) {
         ans.clear();
         vector<int>v;
-        com(n,k,v);
+        go(n,k,v);
         return ans;
     }
 };
