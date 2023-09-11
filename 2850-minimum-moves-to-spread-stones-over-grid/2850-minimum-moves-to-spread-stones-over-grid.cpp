88e44fec204c2ac38d858c98f79cdc809ee8637a
Biswajit Rout <101084372+routb68@users.noreply.github.com>
2023-09-11 17:04:43 +0530

    Time: 12 ms (100.00%), Space: 20.5 MB (14.29%) - LeetHub

--- 2850-minimum-moves-to-spread-stones-over-grid/2850-minimum-moves-to-spread-stones-over-grid.cpp
+++ 2850-minimum-moves-to-spread-stones-over-grid/2850-minimum-moves-to-spread-stones-over-grid.cpp
@@ -1,52 +1,40 @@
 class Solution {
 public:
-    using pii = pair<int, int>;
-    vector<pii> zeros, non_one;
-    int dp[8][256];
-    int N;
-
-    int minimumMoves(vector<vector<int>>& grid) {
-        zeros.clear();
-        non_one.clear();
-        for (int i = 0; i < 3; ++i) {
-            for (int j = 0; j < 3; ++j) {
-                if (grid[i][j] == 0) {
-                    zeros.emplace_back(i, j);
-                } else if (grid[i][j] != 1) {
-                    for (int k = 0; k < grid[i][j] - 1; ++k) {
-                        non_one.emplace_back(i, j);
+    vector<pair<int,int>>z,nz;
+    unordered_map<int,int>dp[8];
+    
+    int go(int idx,int mask){
+        if(idx==z.size()){
+            return 0;
+        }
+        if(dp[idx].find(mask)!=dp[idx].end()) return dp[idx][mask];
+        int cost = INT_MAX;
+        for(int i=0;i<z.size();++i){
+            if((mask & (1<<i))==0){
+                int tempcost = getcost(idx,i);
+                tempcost += go(idx+1,mask |(1<<i));
+                cost = min(cost,tempcost);
+            }
+        }
+        return dp[idx][mask]=cost;
+    }
+    
+    int getcost(int a,int b){
+        return abs(z[a].first-nz[b].first)+ abs(z[a].second-nz[b].second);
+    }
+    
+    int minimumMoves(vector<vector<int>>& gd) {
+        for(int i=0;i<3;++i){
+            for(int j=0;j<3;++j){
+                if(gd[i][j]==0){
+                    z.push_back(make_pair(i,j));
+                }else{
+                    for(int k=0;k<gd[i][j]-1;++k){
+                        nz.push_back(make_pair(i,j));
                     }
                 }
             }
         }
-        N = ((int) zeros.size());
-        // MASK = (1 << N) - 1;
-        memset(dp, -1, sizeof(dp));
-        int ans = solve(0, 0);
-        return ans;
+        return go(0,0);
     }
-
-    int solve(int index, int mask) {
-        if (index == N) return 0;
-        if (dp[index][mask] != -1) return dp[index][mask];
-        int cost = INT_MAX;
-        for (int i = 0; i < N; ++i) {
-            if ((mask & (1 << i)) == 0) {
-                int current_cost = get_cost(index, i);
-                current_cost += solve(index + 1, mask ^ (1 << i));
-                cost = min(cost, current_cost);
-            }
-        }
-        return dp[index][mask] = cost;
-    }
-    
-    
-    
-
-    int get_cost(int p, int q) { 
-        return abs(zeros[p].first - non_one[q].first) + 
-                          abs(zeros[p].second - non_one[q].second); }
-    
-    
-    
 };
