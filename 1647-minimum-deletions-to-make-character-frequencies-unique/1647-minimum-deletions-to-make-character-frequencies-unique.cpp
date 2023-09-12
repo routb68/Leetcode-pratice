a9630b7dc2d953dc0e330feddbf78557c731191c
Biswajit Rout <101084372+routb68@users.noreply.github.com>
2023-09-12 11:26:24 +0530

    Time: 47 ms (85.12%), Space: 17.4 MB (37.86%) - LeetHub

---
+++ 1647-minimum-deletions-to-make-character-frequencies-unique/1647-minimum-deletions-to-make-character-frequencies-unique.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int minDeletions(string s) {
+        int charfrq[26];
+        for(int i=0;i<26;++i)charfrq[i]=0;
+        for(char &x:s)++charfrq[x-'a'];
+        unordered_map<int,int>ump;
+        for(int i=0;i<26;++i){
+            if(charfrq[i]){
+                ++ump[charfrq[i]];
+            }
+        }
+        int ans =0;
+        for( auto [x,y]:ump){
+            for(int i=0;i<y-1;++i){
+                int z = x;
+                while(z and ump.find(z)!=ump.end()){
+                    --z;
+                }
+                ans += (x-z);
+                if(z)ump[z]=1;
+            }
+            ump[x]=1;
+        }
+        return ans;
+    }
+};
