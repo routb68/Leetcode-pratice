3ab91139ea3b7ae999d2d5d9c6f3c4c8f277f78e
Biswajit Rout <101084372+routb68@users.noreply.github.com>
2023-09-12 12:43:37 +0530

    Time: 321 ms (46.42%), Space: 140.6 MB (46.41%) - LeetHub

---
+++ 2170-minimum-operations-to-make-the-array-alternating/2170-minimum-operations-to-make-the-array-alternating.cpp
@@ -0,0 +1,53 @@
+class Solution {
+public:
+    int minimumOperations(vector<int>& nums) {
+        unordered_map<int,int>umpod,umpev;
+        for(int i=0;i<nums.size();++i){
+            if(i&1){
+                ++umpod[nums[i]];
+            }else{
+                ++umpev[nums[i]];
+            }
+        }
+        priority_queue<pair<int,int>,vector<pair<int,int>>, 
+                                      greater<pair<int,int>> >pqod,pqev;
+        for(auto [x,y]:umpod){
+            // cout<<y<<" " <<x<<endl;
+            pqod.push(make_pair(y,x));
+            if(pqod.size()>5){
+                pqod.pop();
+            }
+        }
+        // cout<<endl;
+        for(auto [x,y]:umpev){
+            // cout<<y<<" " <<x<<endl;
+            pqev.push(make_pair(y,x));
+            if(pqev.size()>5){
+                pqev.pop();
+            }
+        }
+        vector<pair<int,int>>od,ev;
+        while(pqod.size()){
+            od.push_back(pqod.top());
+            pqod.pop();
+        }
+        while(pqev.size()){
+            ev.push_back(pqev.top());
+            pqev.pop();
+        }
+        int ans =INT_MAX,n=nums.size();
+        for(int i=0;i<od.size();++i){
+            for(int j=0;j<ev.size();++j){
+                if(od[i].second!=ev[j].second){
+                    ans = min(ans,n-od[i].first-ev[j].first);
+                }
+                // if(i==0){
+                //     // cout<<ev[j].first<<" "<<ev[j].second<<endl;
+                // }
+            }
+            // cout<<od[i].first<<" "<<od[i].second<<endl;
+        }
+        if(ans==INT_MAX) return n/2;
+        return ans;
+    }
+};
