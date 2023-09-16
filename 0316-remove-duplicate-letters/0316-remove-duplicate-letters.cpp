46ee66d90bddff48bf3cd0c48223ef8e980979dd
Biswajit Rout <101084372+routb68@users.noreply.github.com>
2023-09-17 00:49:03 +0530

    Time: 797 ms (8.32%), Space: 12.8 MB (5.28%) - LeetHub

---
+++ 0316-remove-duplicate-letters/0316-remove-duplicate-letters.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    
+    string go(string &s,int pos,vector<bool>&v){
+        if(pos==s.size()){
+            return "";
+        }
+        int x =pos;
+        int char_fr[26];
+        for(int i=0;i<26;++i)char_fr[i]=0;
+        for(int i=pos;i<s.size();++i){
+            if(v[s[i]-'a'])continue;
+            ++char_fr[s[i]-'a'];
+        }
+        for(int i=pos;i<s.size();++i){
+            if(v[s[i]-'a'])continue;
+            if(s[i]<s[x])x=i;
+            --char_fr[s[i]-'a'];
+            if(char_fr[s[i]-'a']==0)break;
+        }
+        if(v[s[x]-'a']) return go(s,x+1,v);
+        v[s[x]-'a']=true;
+        return s[x]+go(s,x+1,v);
+    }
+    string removeDuplicateLetters(string s) {
+        vector<bool>v(26);
+        for(int i=0;i<26;++i)v[i]=false;
+        return go(s,0,v);
+    }
+};
