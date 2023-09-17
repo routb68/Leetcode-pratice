38020bfb45d67eb3274390b61cdbb01a207b73fc
Biswajit Rout <101084372+routb68@users.noreply.github.com>
2023-09-17 18:41:31 +0530

    Time: 180 ms (12.50%), Space: 164 MB (87.50%) - LeetHub

---
+++ 2856-minimum-array-length-after-pair-removals/2856-minimum-array-length-after-pair-removals.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int minLengthAfterRemovals(vector<int>& nums) {
+        unordered_map<int,int>ump;
+        for(int &x:nums)++ump[x];
+        int cnt =0;
+        for (auto [x,y]:ump)cnt =max(cnt,y);
+        if(cnt<=nums.size()/2){
+            return nums.size() %2;
+        }
+        return cnt+cnt-nums.size();
+    }
+};
