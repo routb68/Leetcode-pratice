b71ee0ac80d3e5bce18f3cf01dd1cc82c356c903
Biswajit Rout <101084372+routb68@users.noreply.github.com>
2023-09-21 10:26:09 +0530

    Time: 0 ms (100.00%), Space: 6.8 MB (6.46%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.cpp
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -1,27 +1,18 @@
 class Solution {
 public:
+    bool check(char b,char a){
+        return {(a=='(' and b==')') or (a=='{' and b=='}') or (a=='[' and b==']')};
+    }
     bool isValid(string s) {
         stack<char>st;
-        for(char &x:s){
-            if(x=='(' or x=='{' or x=='['){
-                st.push(x);
-                continue;
+        for(int i=0;i<s.size();++i){
+            if(s[i]=='(' or s[i]=='[' or s[i]=='{'){
+                st.push(s[i]);
+            }else{
+                if(st.size()==0) return false;
+                if(check(s[i],st.top())==false) return false;
+                st.pop();
             }
-            if(st.size()==0) return false;
-            if(x==')'){
-                if(st.top()!='(') 
-                    return false;
-            }
-            if( x=='}'){
-                if(st.top()!='{')
-                    return false;
-            }
-            if(x==']'){
-                if(st.top()!='['){
-                    return false;
-                }
-            }
-            st.pop();
         }
         return st.size()==0;
     }
