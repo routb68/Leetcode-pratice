eae9800c0a67ca5bea4e7ac7183802441b388553
Biswajit Rout <101084372+routb68@users.noreply.github.com>
2023-09-22 15:56:26 +0530

    Time: 0 ms (100.00%), Space: 7.1 MB (7.06%) - LeetHub

--- 0392-is-subsequence/0392-is-subsequence.cpp
+++ 0392-is-subsequence/0392-is-subsequence.cpp
@@ -1,14 +1,18 @@
 class Solution {
 public:
     bool isSubsequence(string s, string t) {
-        int i=0,j=0;
-        while(i<s.length() and j<t.length()){
-            if(s[i]==t[j]){
-                ++i;++j;
-            }else{
-                ++j;
-            }
+        vector<int>v[26];
+        for(int i=0;i<t.size();++i){
+            v[t[i]-'a'].push_back(i);
         }
-        return i==s.length();
+        int pre=-1;
+        for(int i=0;i<s.size();++i){
+            if(v[s[i]-'a'].size()==0 or v[s[i]-'a'].back()<pre) return false;
+            int idx= lower_bound(begin(v[s[i]-'a']),end(v[s[i]-'a']),pre+1)-
+                      v[s[i]-'a'].begin();
+            if(idx==v[s[i]-'a'].size()) return false;
+            pre = v[s[i]-'a'][idx];
+        }
+        return true;
     }
 };
