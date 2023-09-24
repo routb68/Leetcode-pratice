f686818dfdfd2a7e46af1abde2b30208fb6cbe86
Biswajit Rout <101084372+routb68@users.noreply.github.com>
2023-09-24 12:55:10 +0530

    Time: 0 ms (100.00%), Space: 6.6 MB (7.02%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.cpp
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -1,11 +1,10 @@
 class Solution {
 public:
     int fib(int n) {
-        int arr[n+2];
-        arr[0]=0,arr[1]=1;
-        for(int i=2;i<=n;++i){
-            arr[i]=arr[i-1]+arr[i-2];
-        }
-        return arr[n];
+        if(n<2) return n;
+        int dp[n+1];
+        dp[0]=0;dp[1]=1;
+        for(int i=2;i<=n;++i)dp[i]=dp[i-1]+dp[i-2];
+        return dp[n];
     }
 };
