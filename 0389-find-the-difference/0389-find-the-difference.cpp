2613dde1ab7480fe5561ff4af9c4389b9374876f
Biswajit Rout <101084372+routb68@users.noreply.github.com>
2023-09-25 11:48:53 +0530

    Time: 4 ms (48.11%), Space: 7 MB (16.46%) - LeetHub

--- 0389-find-the-difference/0389-find-the-difference.cpp
+++ 0389-find-the-difference/0389-find-the-difference.cpp
@@ -2,8 +2,8 @@ class Solution {
 public:
     char findTheDifference(string s, string t) {
         int x =0;
-        for(auto &a:s) x^=a;
-        for(auto &b:t) x^=b;
+        for(char &a:s) x^=a;
+        for(char &b:t) x^=b;
         return x;
     }
 };
