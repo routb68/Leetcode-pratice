9efdf49f4d89ed93b6557627e4c6e7f592140633
Biswajit Rout <101084372+routb68@users.noreply.github.com>
2023-10-03 21:35:43 +0530

    Time: 17 ms (59.84%), Space: 13.5 MB (77.91%) - LeetHub

--- 0210-course-schedule-ii/0210-course-schedule-ii.cpp
+++ 0210-course-schedule-ii/0210-course-schedule-ii.cpp
@@ -1,24 +1,22 @@
 class Solution {
 public:
-    vector<int> findOrder(int nc, vector<vector<int>>& prqs) {
-         int indegree[nc];
-        vector<int> adj[nc];
+    vector<int> findOrder(int nc, vector<vector<int>>& pqs) {
+        int indegree[nc];
         for(int i=0;i<nc;++i)indegree[i]=0;
-        for(int i=0;i<prqs.size();++i){
-            ++indegree[prqs[i][0]];
-            adj[prqs[i][1]].push_back(prqs[i][0]);
+        vector<int>adj[nc];
+        for(int i=0;i<pqs.size();++i){
+            ++indegree[pqs[i][1]];
+            adj[pqs[i][0]].push_back(pqs[i][1]);
         }
+        vector<int>ans;
         queue<int>q;
         for(int i=0;i<nc;++i){
             if(indegree[i]==0){
                 q.push(i);
             }
         }
-        int cnt =0;
-        vector<int>ans;
         while(q.size()){
             int temp=q.front();q.pop();
-            ++cnt;
             ans.push_back(temp);
             for(int &x:adj[temp]){
                 --indegree[x];
@@ -27,7 +25,8 @@ public:
                 }
             }
         }
-        if(nc==cnt) return ans;
-        return {};
+        if(ans.size()!=nc) return {};
+        reverse(begin(ans),end(ans));
+        return ans;
     }
 };
