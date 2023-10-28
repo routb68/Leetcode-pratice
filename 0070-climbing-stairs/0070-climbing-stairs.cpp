1377a8915489db326d5872c06ae2d47f8eafd5a6
Biswajit Rout <101084372+routb68@users.noreply.github.com>
2023-10-28 12:32:57 +0530

    Time: 0 ms (100%), Space: 6.5 MB (22.58%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,11 +1,15 @@
 class Solution {
 public:
     int climbStairs(int n) {
-        vector<int>v(n+2);
-        v[0]=0;
-        v[1]=1;
-        v[2]=2;
-        for(int i=3;i<=n;++i) v[i]= v[i-1]+v[i-2];
-        return v[n];
+        vector<int>dp(n+1);
+        dp[0]=1;
+        for(int i=1;i<=n;++i){
+            if(i>=2){
+                dp[i]= dp[i-1]+dp[i-2];
+            }else{
+                dp[i]=dp[i-1];
+            }
+        }
+        return dp[n];
     }
 };
