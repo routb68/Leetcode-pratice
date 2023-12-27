111b1c41f08d68a7925bc7256c5ec799ccee7a94
Biswajit Rout <101084372+routb68@users.noreply.github.com>
2023-12-27 11:46:14 +0530

    Time: 10 ms (35.62%), Space: 24.2 MB (88.96%) - LeetHub

---
+++ 2974-minimum-number-game/2974-minimum-number-game.py
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    vector<int> numberGame(vector<int>& nums) {
+        sort(begin(nums),end(nums));
+        for(int i=0;i<nums.size();i=i+2){
+            swap(nums[i],nums[i+1]);
+        }
+        return nums;
+    }
+};
